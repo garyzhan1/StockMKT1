d399893efb66b512ebf5a2a56565157d9837f1c6
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2023-12-02 00:52:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,7 @@ df = pd.DataFrame(
     columns=('column %d' % (i+1) for i in range(5))
 )
 
-st.table(df)
 st.dataframe(df.style.highlight_max(axis=0))
-
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
-
-chart_data = pd.DataFrame(
-    np.random.randn(50, 3),
-    columns = ["a", "b", "c"])
-st.bar_chart(chart_data)
 
 my_slider = st.slider("Customer satisfaction",0,100,50,1)
 if my_slider:f"stock price changes by {my_slider * 2 + 0.5 + my_slider ** 1.5} when customer satisfaction is {my_slider}"
@@ -29,6 +18,15 @@ if my_slider:f"stock prices by {my_slider * 2 + 0.5 + my_slider ** 1.5} when the
 my_slider = st.slider("brand",0,100,50,1)
 if my_slider:f"stock price rises by {my_slider * 2 + 0.5 + my_slider ** 1.5} when brand value is {my_slider}"
 
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
+
+chart_data = pd.DataFrame(
+    np.random.randn(50, 3),
+    columns = ["a", "b", "c"])
+st.bar_chart(chart_data)
 
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
