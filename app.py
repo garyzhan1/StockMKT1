962a7a3b48da73ab172c8a23c04c84171a27be4e
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2023-12-02 21:43:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import streamlit as st  # pip install streamlit
 import numpy as np
 
 
-my_slider = st.slider("Customer satisfaction",0,100,50,1)
-my_slider2 = st.slider("new product",0,20,10,1)
-my_slider3 = st.slider("brand",0,100,50,1)
-if my_slider:f"stock price changes by {my_slider * 2 + 0.5 + my_slider2 ** 1.5/my_slider3} when customer satisfaction is {my_slider}"
+my_slider = st.slider("a pioneering innovation",0,10,0,1)
+my_slider2 = st.slider("new market entries",0,20,0,1)
+my_slider3 = st.slider("minor updates",0,100,0,1)
+my_slider4 = st.slider("advertising for new product introduction (million USD)",0,200,0,1)
+my_slider5 = st.slider("qaulity improvement (%)",0,100%,0,1%)
+if my_slider:f"stock price changes by {my_slider * 4.28% + my_slider2 * 0.98% + my_slider3 * 0.55% + my_slider4 * 0.1% + my_slider5 * 4.2%} when customer satisfaction is {my_slider}"
 
 st.write("data in a table:")
 st.write(pd.DataFrame({
