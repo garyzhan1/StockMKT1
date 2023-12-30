0ee9ee6860f6d70e25ecd0953e198ae21104e275
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2023-12-30 22:35:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ container.my_slider2 = st.slider("new market entries",0,20,0,1)
 container.my_slider3 = st.slider("minor updates",0,100,0,1)
 container.my_slider4 = st.slider("advertising for new product introduction (million USD)",0,200,0,1)
 container.my_slider5 = st.slider("qaulity improvement (%)",0,100,0,1)
-container.if my_slider:f"stock price will change by {my_slider * 0.0428 + my_slider2 * 0.0098 + my_slider3 * 0.0055 + my_slider4 * 0.0001 + my_slider5 * 0.042}"
+if my_slider:f"stock price will change by {my_slider * 0.0428 + my_slider2 * 0.0098 + my_slider3 * 0.0055 + my_slider4 * 0.0001 + my_slider5 * 0.042}"
 
 st.header('', divider='rainbow')
     
