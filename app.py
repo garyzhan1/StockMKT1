a75466ed18e1355e83b30f1b3d4341441a47d8ce
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2023-12-30 23:11:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,24 @@ import pandas as pd  # pip install pandas openpyxl
 import streamlit as st  # pip install streamlit
 import numpy as np
 
-st.header('Product-related factors', divider='rainbow')
+tab1, tab2, tab3 = st.tabs(["product", "trademark", "cs"])
 
-my_slider = st.slider("a pioneering innovation",0,10,0,1)
-my_slider2 = st.slider("new market entries",0,20,0,1)
-my_slider3 = st.slider("minor updates",0,100,0,1)
-my_slider4 = st.slider("advertising for new product introduction (million USD)",0,200,0,1)
-my_slider5 = st.slider("qaulity improvement (%)",0,100,0,1)
-if my_slider:f"stock price will change by {my_slider * 0.0428 + my_slider2 * 0.0098 + my_slider3 * 0.0055 + my_slider4 * 0.0001 + my_slider5 * 0.042}"
+with tab1:
+   st.header('Product-related factors', divider='rainbow')
+   my_slider = st.slider("a pioneering innovation",0,10,0,1)
+   my_slider2 = st.slider("new market entries",0,20,0,1)
+   my_slider3 = st.slider("minor updates",0,100,0,1)
+   my_slider4 = st.slider("advertising for new product introduction (million USD)",0,200,0,1)
+   my_slider5 = st.slider("qaulity improvement (%)",0,100,0,1)
+   if my_slider:f"stock price will change by {my_slider * 0.0428 + my_slider2 * 0.0098 + my_slider3 * 0.0055 + my_slider4 * 0.0001 + my_slider5 * 0.042}"
 
-st.header('trademark', divider='rainbow')
-
-my_slider6 = st.slider("a brand-association trade mark",0,10,0,1)
-if my_slider:f"stock price will change by {my_slider6 * 0.003}"
-
-col4, col5 = st.columns(2)
-col4.write("Krasnikov, A., Mishra, S., & Orozco, D. (2009).: Evaluating the financial impact of branding using trademarks: A framework and empirical evidence.")
-col5.write("Krasnikov, A., Mishra, S., & Orozco, D. (2009).: Evaluating the financial impact of branding using trademarks: A framework and empirical evidence.")
+with tab2:
+   st.header('trademark', divider='rainbow')
+   my_slider6 = st.slider("a brand-association trade mark",0,10,0,1)
+   if my_slider:f"stock price will change by {my_slider6 * 0.003}"
+   col4, col5 = st.columns(2)
+   col4.write("Krasnikov, A., Mishra, S., & Orozco, D. (2009).: Evaluating the financial impact of branding using trademarks: A framework and empirical evidence.")
+   col5.write("Krasnikov, A., Mishra, S., & Orozco, D. (2009).: Evaluating the financial impact of branding using trademarks: A framework and empirical evidence.")
 
 col1, col2, col3 = st.columns(3)
 col1.metric("cusotmer satisfaction", "1.5", "5%")
