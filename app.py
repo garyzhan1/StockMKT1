5b2bd103dc5a041f8ea1042245216cc60cb6bfc8
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2023-12-31 12:07:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ with tab1:
    my_slider3 = st.slider("minor updates",0,100,0,1)
    my_slider4 = st.slider("advertising for new product introduction (million USD)",0,200,0,1)
    my_slider5 = st.slider("qaulity improvement (%)",0,100,0,1)
-   if my_slider:f"stock price will change by {my_slider * 4.28 + my_slider2 * 0.98 + my_slider3 * 0.55 + my_slider4 * 0.01 + my_slider5 * 4.2}%"
+   with st.container(border=True):
+      if my_slider:f"stock price will change by {my_slider * 4.28 + my_slider2 * 0.98 + my_slider3 * 0.55 + my_slider4 * 0.01 + my_slider5 * 4.2}%"
    col6, col7 = st.columns(2)
    col6.write("The parameter used in the prediction was developed on the basis of:")
    col7.write("Krasnikov, A., Mishra, S., & Orozco, D. (2009). Evaluating the financial impact of branding using trademarks: A framework and empirical evidence. Journal of Marketing, 73(6), 154-166.")
@@ -20,7 +21,8 @@ with tab2:
    st.header('Customer satisfaction & CSR', divider='rainbow')
    my_slider7 = st.slider("Customer satisfaction",0,100,0,1)
    my_slider8 = st.slider("CSR",0,100,0,1)
-   if my_slider:f"for each unit of change in customer satisfaction or CSR, stock price will change by {my_slider7 * 17 + my_slider8 * 14}%"
+   with st.container(border=True):
+      if my_slider:f"for each unit of change in customer satisfaction or CSR, stock price will change by {my_slider7 * 17 + my_slider8 * 14}%"
    col6, col7 = st.columns(2)
    col6.write("The parameter used in the prediction was developed on the basis of the American Customer Satisfaction Index (ACSI), Fortune America's Most Admired Corporations (FAMA).")
    col7.write("Source: Luo, X., & Bhattacharya, C. B. (2006). Corporate social responsibility, customer satisfaction, and market value. Journal of marketing, 70(4), 1-18.")
