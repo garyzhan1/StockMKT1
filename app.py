7fc66fe6d6e1059b384232b6c3762d1d850f34b2
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2024-01-03 12:10:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,18 +13,18 @@ with tab1:
    my_slider1 = st.slider("a pioneering innovation开创性创新的数量",0,10,0,1)
    my_slider2 = st.slider("new market entries进入了几个新产品市场",0,20,0,1)
    my_slider3 = st.slider("minor updates微创新的数量",0,100,0,1)
-   my_slider4 = st.slider("advertising for new product introduction (million USD)",0,200,0,1)
+   my_slider4 = st.slider("advertising for new product introduction (million USD) 用于新产品推广的广告费（百万美金）",0,200,0,1)
    my_slider5 = st.slider("qaulity improvement (%)产品质量改进百分比",0,100,0,1)
    with st.container(border=True):
       if my_slider:f"stock price will change by {my_slider1 * 4.28 + my_slider2 * 0.98 + my_slider3 * 0.55 + my_slider4 * 0.01 + my_slider5 * 4.2}%"
    col6, col7 = st.columns(2)
-   col6.write("The parameter used in the prediction was developed on the basis of:预测模型中的参数来自右边这篇论文：")
+   col6.write("The parameter used in the prediction was developed on the basis of: 预测模型中的参数来自论文：")
    col7.write("Krasnikov, A., Mishra, S., & Orozco, D. (2009). Evaluating the financial impact of branding using trademarks: A framework and empirical evidence. Journal of Marketing, 73(6), 154-166.")
 
 with tab2:
    st.header('Customer satisfaction & CSR', divider='rainbow')
-   my_slider7 = st.slider("Customer satisfaction",0,100,0,1)
-   my_slider8 = st.slider("CSR",0,100,0,1)
+   my_slider7 = st.slider("Customer satisfaction 消费者满意度",0,100,0,1)
+   my_slider8 = st.slider("CSR 企业社会责任",0,100,0,1)
    with st.container(border=True):
       if my_slider:f"for each unit of change in customer satisfaction or CSR, stock price will change by 股价将会变动 {my_slider7 * 17 + my_slider8 * 14}%"
    col6, col7 = st.columns(2)
@@ -32,7 +32,7 @@ with tab2:
    col7.write("Source: Luo, X., & Bhattacharya, C. B. (2006). Corporate social responsibility, customer satisfaction, and market value. Journal of marketing, 70(4), 1-18.")
 
 with tab3:
-   st.header('Trademark', divider='rainbow')
+   st.header('Trademark 商标', divider='rainbow')
    my_slider6 = st.slider("number of trademarks specifying brand attribute or image 带有品牌属性或形象的商标数量",0,10,0,1)
    with st.container(border=True):
       if my_slider:f"stock price will change by股价将会变动 {my_slider6 * 0.3}%"
