f99662889eaebcd26f373934ecb59eb193fad8cf
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2024-01-03 10:52:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ import pandas as pd  # pip install pandas openpyxl
 import streamlit as st  # pip install streamlit
 import numpy as np
 
+
+
+
 tab1, tab2, tab3 = st.tabs(["Product", "Customer satisfaction & CSR", "Trademark"])
 
 with tab1:
    st.header('Product-related factor', divider='rainbow')
-   my_slider = st.slider("a pioneering innovation *",0,10,0,1)
+   my_slider1 = st.slider("a pioneering innovation *",0,10,0,1)
    my_slider2 = st.slider("new market entries",0,20,0,1)
    my_slider3 = st.slider("minor updates",0,100,0,1)
    my_slider4 = st.slider("advertising for new product introduction (million USD)",0,200,0,1)
    my_slider5 = st.slider("qaulity improvement (%)",0,100,0,1)
    with st.container(border=True):
-      if my_slider:f"stock price will change by {my_slider * 4.28 + my_slider2 * 0.98 + my_slider3 * 0.55 + my_slider4 * 0.01 + my_slider5 * 4.2}%"
+      if my_slider:f"stock price will change by {my_slider1 * 4.28 + my_slider2 * 0.98 + my_slider3 * 0.55 + my_slider4 * 0.01 + my_slider5 * 4.2}%"
    col6, col7 = st.columns(2)
    col6.write("The parameter used in the prediction was developed on the basis of:")
    col7.write("Krasnikov, A., Mishra, S., & Orozco, D. (2009). Evaluating the financial impact of branding using trademarks: A framework and empirical evidence. Journal of Marketing, 73(6), 154-166.")
