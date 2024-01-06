8f124b3902e849a4252671d689378a5729cb4f6e
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2024-01-06 10:13:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ with tab2:
    with st.container(border=True):
       if my_slider:f"for each unit of change in customer satisfaction or CSR, stock price will change by {my_slider7 * 17 + my_slider8 * 14}%"
    col6, col7 = st.columns(2)
-   col6.write("The parameter used in the prediction was developed on the basis of the American Customer Satisfaction Index (ACSI), Fortune America's Most Admired Corporations (FAMA).预测模型中的参数来自美国消费者满意度指数和福布斯美国最受赞赏公司榜单")
+   col6.write("The parameter used in the prediction was developed on the basis of the American Customer Satisfaction Index (ACSI), Fortune America's Most Admired Corporations (FAMA).")
    col7.write("Source: Luo, X., & Bhattacharya, C. B. (2006). Corporate social responsibility, customer satisfaction, and market value. Journal of marketing, 70(4), 1-18.")
 
 with tab3:
