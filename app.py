11fb8e9932bd83491670153c1692b1c791dc9a9d
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2025-01-04 23:47:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ with tab0:
       stock_code = col2.text_input("股票代码")
    
    # Create sliders for customer satisfaction and CSR
-   my_slider7 = st.slider("Customer satisfaction", 0, 100, 0, 1)
-   my_slider8 = st.slider("CSR", 0, 100, 0, 1)
+   my_slider9 = st.slider("Customer satisfaction", 0, 100, 0, 1)
+   my_slider10 = st.slider("CSR", 0, 100, 0, 1)
    
    # Create a container to display the result
    with st.container(border=True):
       # Check if the sliders have been moved
-      if my_slider7 or my_slider8:
+      if my_slider9 or my_slider10:
          # Display the result
          st.write(f"For each unit of change in customer satisfaction or CSR, {firm_name} ({stock_code}) will change by {my_slider7 * 17 + my_slider8 * 14}%")
    
