8a7247224679155b4c98127f5559f288a352bbfc
Gary Zhan <64673971+garyzhan1@users.noreply.github.com>
2025-05-07 11:35:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,8 +71,8 @@ with tab2:
       col1, col2 = st.columns(2)
       
       # Create input fields for firm name and stock code
-      firm_name = col1.text_input("Firm Name")
-      stock_code = col2.text_input("Stock Code")
+      firm_name = col1.text_input("Firm Code")
+      stock_code = col2.text_input("Stock")
    
    # Create sliders for customer satisfaction and CSR
    my_slider7 = st.slider("Customer satisfaction", 0, 100, 0, 1)
